913b86703a7e92157db488a87bfb1bdc8a956eec
Dalton <>
2018-02-14 10:53:12 +0000

    Tutorial 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule} from '@angular/forms'; //<--NgModel lives here
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-import { MessagesComponent } from './messages/messages.component';
+import { HeroService } from './hero.service';
 import { MessageService } from './message.service';
-
+import { MessagesComponent } from './messages/messages.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +20,10 @@ import { MessageService } from './message.service';
     BrowserModule,
     FormsModule
   ],
-  providers: [MessageService],
-  bootstrap: [AppComponent]
+  providers: [
+    HeroService,
+    MessageService
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
