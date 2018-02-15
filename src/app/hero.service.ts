107c0f103e72cd5413d013dabd4e432e1a3ecf82
1218170 <d.sutherland@wlv.ac.uk>
2018-02-15 15:34:48 +0000

    messages2

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -12,7 +12,7 @@ export class HeroService {
 
   constructor(private messageService: MessageService) { }
 
-  getHeroes(): Observable<Hero[]> {
+  getHero(): Observable<Hero[]> {
     // Todo: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
